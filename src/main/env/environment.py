61ae20a2959612532581f32d4d73308a4b2e6437
Eliah7 <eliahmbwilo7@gmail.com>
2020-10-22 12:06:25 +0300

    Initilizing empty environment

---
+++ src/main/env/environment.py
@@ -0,0 +1,14 @@
+# Authored by elia on 22/10/2020 
+
+# Feature: #Enter feature name here
+# Enter feature description here
+
+# Scenario: # Enter scenario name here
+"""
+
+"""
+import Gym as Gym
+
+
+class Environment(Gym.Environment):
+    pass
---
+++ src/tests/environment_tests.py
@@ -0,0 +1,15 @@
+# Authored by elia on 22/10/2020 
+
+# Feature: #Enter feature name here
+# Enter feature description here
+
+# Scenario: # Enter scenario name here
+"""
+
+"""
+
+from unittest import TestCase
+
+class TestEnvironment(TestCase):
+    def testCreateEnvironment(self):
+        pass
