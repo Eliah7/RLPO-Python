7d0c50db32032450323d565c23f4d8fb28db52ee
Eliah7 <eliahmbwilo7@gmail.com>
2020-12-22 16:36:25 +0300

    Refractor environment state

--- src/main/env/environment.py
+++ src/main/env/environment.py
@@ -34,41 +34,37 @@ class Environment(gym.Env):
         self.n_nodes = n_nodes
         self.load_data = load_data
         self.line_data = line_data
-        self.max_action = 2 ** self.n_nodes
         self.reward_range = spaces.Box(low=0, high=1000, shape=(1,)) #spaces.Box(np.array(0), np.array(100))
-        # 8589934592
+
+        high = np.array([np.inf] * self.n_nodes)
+        self.observation_space = spaces.Box(-high, high)
+
         self.action_space = spaces.Discrete(self.n_nodes+1)
         self.num_actions = 0
 
         # (sum_power_assigned, sum_status, sum_priority) : defines state
         #  implement value-function approximation to summarize node information
-        self.observation_space = spaces.Box(np.array((-10000, -10000, -10000)), np.array((10000, 10000, 10000)))
+        # self.observation_space = spaces.Box(np.array((-10000, -10000, -10000)), np.array((10000, 10000, 10000)))
 
         self.done = False
 
     def get_remaining_power(self, max_capacity, load_data):
         power_assigned = np.sum(load_data[:, 1] * load_data[:, 4])
-
         return max_capacity - power_assigned
 
     def step(self, action):
         # Execute one time step within the environment
-        # action = int(action[0])
+
         obs = self.get_observation(action)
         reward = self.reward()
-        # if self.power_assigned() >= self.max_capacity:
-        #     reward -= 5
-        #     self.done = True
-        # else:
+
+        power_values_from_dlf, _ = dlf_analyse(self)
 
         if self.num_actions == 10:
             # print("DONE")
             self.done = True
         else:
             self.done = False
-            # print(self.num_actions)
-            if self.power_assigned() >= self.max_capacity:
-                reward -= 5
             self.num_actions += 1
 
         self.done = True
@@ -79,26 +75,21 @@ class Environment(gym.Env):
         self.load_data = loadData
         self.line_data = lineData
         self.num_actions = 0
-        # self.remain_power = self.get_remaining_power(self.max_capacity, self.load_data)
-        # self.power_assigned = np.sum(self.load_data[:, 1] * self.load_data[:, 4])
+
         self.done = False
 
         return self.get_observation()
 
     def get_observation(self, action=np.inf):
-        # assert (2 ** self.n_nodes) >= action >= 0, 'Action can not exceed nodes count'
         if action == np.inf :
             return self.current_state()
         else:
-            # print("===> Action : {} in binary: {}".format(action, get_bin_str_with_max_count(action, self.n_nodes)))
-
-            # print("ACTION: " + action_str)
             if action <= self.n_nodes:
                 self.act_from_num(action=action)
             else:
                 action_str = get_bin_str_with_max_count(action, self.n_nodes)
                 self.act(action_str)
-            print("CURRENT STATE: " + str(self.load_data[:, 4]))
+            # print("CURRENT STATE: " + str(self.load_data[:, 4]))
 
             return self.current_state()
 
@@ -113,6 +104,8 @@ class Environment(gym.Env):
     def act(self, action_str):
         for action in range(len(action_str)):
             self.load_data[:, 4][action] = int(action_str[action])
+
+        self.load_data[:, 4][0] = 1
         return
 
     def current_state(self):
@@ -120,7 +113,9 @@ class Environment(gym.Env):
         sum_statuses = np.sum(self.load_data[:, 4])
         sum_priorities = np.sum(self.load_data[:, 5])
 
-        return np.array([power_assigned, sum_statuses, sum_priorities])
+        # return np.array([power_assigned, sum_statuses, sum_priorities])
+        # return np.array(self.load_data[:, 1] * self.load_data[:, 4] * np.square(self.load_data[:, 5]))
+        return np.array(self.load_data[:, 4])
 
     def reward(self):
         status_reward = np.sum(self.load_data[:, 1] * self.load_data[:, 4] * np.square(self.load_data[:, 5])) ** 0.4 # positive rewards
@@ -129,15 +124,13 @@ class Environment(gym.Env):
         power_values_from_dlf, _ = dlf_analyse(self)
 
         power_values_from_dlf = np.array(power_values_from_dlf)
-        # 
-        # # if self.load_data[:, 4][0] != 1:
-        # #     return -100
-        #
-        if power_values_from_dlf[(power_values_from_dlf > 1.1)] :
-            return -100 + status_reward
+
+        if power_values_from_dlf[(power_values_from_dlf > 1.1)]:
+            return np.inf
 
         if power_values_from_dlf[(power_values_from_dlf < 0.9)] :
-            return -100 + status_reward
+            return np.inf
+
         return status_reward
 
     def power_assigned(self):
