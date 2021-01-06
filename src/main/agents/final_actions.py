edac337007a051be781bc42133488df86f63cc21
ramaj93 <ramaj93@yahoo.com>
2021-01-06 17:59:37 +0300

    merge

--- src/main/agents/final_actions.py
+++ src/main/agents/final_actions.py
@@ -11,10 +11,25 @@ from stable_baselines import DQN, A2C, PPO2
 from src.main.util.model_utils import evaluate
 from src.main.env.environment import Environment
 
+
+def test_rl_agent(agent, environment):
+    pass
+
+
 if __name__ == '__main__':
     env = Environment(grid_name="bus33")
     # model = A2C.load("./saved_models/a2c")
     model = DQN.load("./saved_models/dqn", env=env)
-    # model.env = env
-    # model = PPO2.load("./saved_models/ppo2")
-    evaluate(model)
+
+    obs = env.reset()
+    print("INPUTS: {}".format(obs))
+    done = False
+
+    while not done:
+        action, _states = model.predict(obs)
+        next_obs, reward, done, info = env.step(action)
+
+    if done:
+
+        print("OUPUT: {}".format(next_obs))
+        print("REWARD: {}".format(reward))
